86ad6aab4f2fed3fd27b222555efdf787d87ebd2
Jayson Nacional <jayson.nacional@proton.me>
2026-05-20 12:32:44 +0000

    add badge styling for status

--- ui/src/Task.jsx
+++ ui/src/Task.jsx
@@ -7,11 +7,25 @@ export default function Task({
 	onEdit,
 	onDelete,
 }) {
+	function renderStatus() {
+		switch (status) {
+			case "completed":
+				return <span className="badge bg-success">{status}</span>;
+			case "in progress":
+			case "break in progress":
+				return <span className="badge bg-warning text-dark">{status}</span>;
+			default:
+				return <span className="badge bg-secondary">{status}</span>;
+		}
+	}
+
 	return (
 		<>
 			<div key={id} className="card m-3" style={{ "width": "36rem" }} >
 				<div className="card-body">
-					<div className="text-secondary">{status}</div>
+					<div className="text-end">
+						{renderStatus()}
+					</div>
 					<h5 className="card-title">{name}</h5>
 					<h6 className="card-subtitle mb-2 text-body-secondary">{taskDuration} min focus</h6>
 					<h6 className="card-subtitle mb-2 text-body-secondary">{breakDuration} min break</h6>
